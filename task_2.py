a0ea4007661a9f59e5bcaf9683f2f8d41a0c876d
NoIdeaWhoIAm <88377656+NoIdeaWhoiA@users.noreply.github.com>
2026-02-06 11:34:18 +0700

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,14 @@
+class userData:
+    def __init__(self, Name, Age):
+        self.user_name = Name
+        self.age = Age
+
+    def PrintInfo(self):
+        print(f"User:{self.user_name},Age:{self.age}")
+    
+    def process_data(DataList):
+        result = []
+        for d in DataList:
+            if d.age > 18:
+                result.append(d.user_name.upper())
+        return result
